473a09ab384d6f96c7f48b114502acf6233ea2aa
sama26 <58335184+sama26@users.noreply.github.com>
2021-06-15 08:55:13 +0100

    first commit - new project

---
+++ sql_queries.py
@@ -0,0 +1,92 @@
+import configparser
+
+
+# CONFIG
+config = configparser.ConfigParser()
+config.read('dwh.cfg')
+
+# DROP TABLES
+
+staging_events_table_drop = "DROP table IF EXISTS staging_events"
+staging_songs_table_drop = "DROP table IF EXISTS staging_songs"
+songplay_table_drop = "DROP table IF EXISTS songplays"
+user_table_drop = "DROP table IF EXISTS users"
+song_table_drop = "DROP table IF EXISTS songs"
+artist_table_drop = "DROP table IF EXISTS artists"
+time_table_drop = "DROP table IF EXISTS time"
+
+# CREATE TABLES
+
+staging_events_table_create = ("CREATE TABLE IF NOT EXISTS staging_events"
+                               "("
+                               "event_id identity(0,1) PRIMARY KEY, "
+                               "artist varchar , "
+                               "auth varchar, "
+                               "first_name varchar, "
+                               "gender char(1), "
+                               "item_in_session smallint, "
+                               "last_name varchar, "
+                               "length real, "
+                               "level char(4),"
+                               "location varchar,"
+                               "method char(3),"
+                               "page varchar,"
+                               "registration double precision,"
+                               "session_id integer,"
+                               "song varchar,"
+                               "status integer,"
+                               "ts bigint,"
+                               "user_agent varchar,"
+                               "userid smallint"
+                               ");"
+                               )
+
+staging_songs_table_create = ("""
+""")
+
+songplay_table_create = ("""
+""")
+
+user_table_create = ("""
+""")
+
+song_table_create = ("""
+""")
+
+artist_table_create = ("""
+""")
+
+time_table_create = ("""
+""")
+
+# STAGING TABLES
+
+staging_events_copy = ("""
+""").format()
+
+staging_songs_copy = ("""
+""").format()
+
+# FINAL TABLES
+
+songplay_table_insert = ("""
+""")
+
+user_table_insert = ("""
+""")
+
+song_table_insert = ("""
+""")
+
+artist_table_insert = ("""
+""")
+
+time_table_insert = ("""
+""")
+
+# QUERY LISTS
+
+create_table_queries = [staging_events_table_create, staging_songs_table_create, songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
+drop_table_queries = [staging_events_table_drop, staging_songs_table_drop, songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
+copy_table_queries = [staging_events_copy, staging_songs_copy]
+insert_table_queries = [songplay_table_insert, user_table_insert, song_table_insert, artist_table_insert, time_table_insert]
